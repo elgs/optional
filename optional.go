9b9479da7ffd12d8aef203bc8f965f50700d1d4f
Qian Chen <elgs1980@hotmail.com>
2022-04-13 22:32:47 -0700

    Update.

--- optional.go
+++ optional.go
@@ -4,10 +4,10 @@ import "log"
 
 type Optional[T any] struct {
 	Data  *T
-	Error *error
+	Error error
 }
 
-func New[T any](data *T, err *error) *Optional[T] {
+func New[T any](data *T, err error) *Optional[T] {
 	return &Optional[T]{
 		Data:  data,
 		Error: err,
