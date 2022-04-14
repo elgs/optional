578858faeba6a725880c3cd98ae9ab272dc6d094
Qian Chen <elgs1980@hotmail.com>
2022-04-13 17:12:20 -0700

    Update.

--- optional.go
+++ optional.go
@@ -1,6 +1,27 @@
 package optional
 
+import "log"
+
 type Optional[T any] struct {
-	Data  T
-	Error error
+	Data  *T
+	Error *error
+}
+
+func New[T any](data *T, err *error) *Optional[T] {
+	return &Optional[T]{
+		Data:  data,
+		Error: err,
+	}
+}
+
+func (this *Optional[T]) LogIfError() {
+	log.Println(this.Error)
+}
+
+func (this *Optional[T]) PanicIfError() {
+	log.Panicln(this.Error)
+}
+
+func (this *Optional[T]) FatalIfError() {
+	log.Fatalln(this.Error)
 }
